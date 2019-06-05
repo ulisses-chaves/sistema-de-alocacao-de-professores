688babb90c71dc8060a4be44ee670e671cb5a4c7
unknown <rodemarck.meloj@ufrpe.br>
2019-06-05 15:49:48 -0300

    repondo segurança, inicio

--- src/main/java/com/Hirukar/Project/Config/WebSecurityConfig.java
+++ src/main/java/com/Hirukar/Project/Config/WebSecurityConfig.java
@@ -5,25 +5,41 @@
  */
 package com.Hirukar.Project.Config;
 
-import org.springframework.context.annotation.Configuration;
-/*import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import com.Hirukar.Project.Models.Users_.Detalhes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 /**
  *
  * @author RODEMARCK
  */
-//@Configuration
-public class WebSecurityConfig/* extends WebSecurityConfigurerAdapter**/{
-    /*private static final String[] ARTEFATOS  =  {
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+    private static final String[] ARTEFATOS  =  {
             "/css/**",
             "/img/**",
             "/fonts/**",
             "/js/**",
-        };
+            "/components/**",
+            "/templates/**"
+    };
+
+    @Autowired
+    private Detalhes detalhes;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth)throws Exception{
+        auth.userDetailsService(detalhes).passwordEncoder(new BCryptPasswordEncoder());
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        
         http
             .authorizeRequests()
                 .antMatchers(ARTEFATOS).permitAll()
@@ -39,5 +55,7 @@ public class WebSecurityConfig/* extends WebSecurityConfigurerAdapter**/{
             .logout()
                 .permitAll();
     }
-    */
+
+
+
 }
